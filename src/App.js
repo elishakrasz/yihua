ac803c6dd644a2ff39fea5a012c39b4a1bbceb12
Elisha Mallard <elishashestov@gmail.com>
2021-03-03 21:48:16 +0200

    sign in component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './components/header/header.component';
 import HomePage from './pages/home/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-const HatsPage = (props) => {
-  console.log(props)
-
-  return (
-    <div>
-    <h1>Hats Page</h1>
-    <p>{props.match.params.hatId}</p>
-  </div>
-  )
-}
 
 class App extends Component {
   render() {
     return <div className='App'>
       <div>
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/hats/:hatId" component={HatsPage} />
+          <Route exact path="/signin" component={SignInAndSignUpPage} />
         </Switch>
       </div>
     </div>;
