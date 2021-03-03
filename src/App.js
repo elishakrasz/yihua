890c3c6fd4617136cddeb5a3f2e7cbcc5cb7a620
Elisha Mallard <elishashestov@gmail.com>
2021-03-03 20:17:11 +0200

    updating shop page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = (props) => {
   console.log(props)
@@ -20,7 +21,7 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/hats" component={HatsPage} />
+          <Route exact path="/shop" component={ShopPage} />
           <Route exact path="/hats/:hatId" component={HatsPage} />
         </Switch>
       </div>
