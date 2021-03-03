62a07b080ae852a57f21d34bd761a59206830efe
elishakrasz <elishashestov@gmail.com>
2021-03-03 15:19:31 +0200

    routing features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home/homepage.component';
+
+const HatsPage = (props) => {
+  console.log(props)
+
+  return (
+    <div>
+    <h1>Hats Page</h1>
+    <p>{props.match.params.hatId}</p>
+  </div>
+  )
+}
 
 class App extends Component {
   render() {
     return <div className='App'>
-      <HomePage />
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/hats" component={HatsPage} />
+          <Route exact path="/hats/:hatId" component={HatsPage} />
+        </Switch>
+      </div>
     </div>;
   }
 }
