475f1cc4f23a9d3d49077ca65f5e83f3fa84737a
elishakrasz <elishashestov@gmail.com>
2021-03-04 08:34:06 +0200

    buttons

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -23,8 +23,8 @@ export default class SignIn extends Component {
   };
 
   handleChange = (e) => {
-      const { value, name } = e.target;
-      this.setState({ [name]: value})
+    const { value, name } = e.target;
+    this.setState({ [name]: value })
   }
   render() {
     return (
@@ -32,24 +32,27 @@ export default class SignIn extends Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <FormInput 
+          <FormInput
             name="email"
             type="email"
             value={this.state.email}
             handleChange={this.handleChange}
             label="email"
-            required 
-            />
-          <FormInput 
-            name="password" 
+            required
+          />
+          <FormInput
+            name="password"
             type="password"
             value={this.state.password}
             handleChange={this.handleChange}
             label="password"
-            required 
-            />
-          <CustomButton type="submit">sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle} >sign in with google</CustomButton>
+            required
+          />
+          <div className="buttons">
+            <CustomButton type="submit">sign in</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>sign in with google</CustomButton>
+          </div>
+
         </form>
       </div>
     );
