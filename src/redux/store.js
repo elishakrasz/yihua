69837b709a481b531f9d5a3ab3c0c688cbad1576
elishakrasz <elishashestov@gmail.com>
2021-03-07 09:02:49 +0200

    finish

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist'
 
 import rootRducer from './root-reducer'
 
 const middlewares = [logger];
 
-const store = createStore(rootRducer, applyMiddleware(...middlewares))
+export const store = createStore(rootRducer, applyMiddleware(...middlewares))
 
-export default store;
+export const persistor = persistStore(store)
+
+export default { store, persistor };
