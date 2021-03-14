c9a61af3dce749f911d3e8e5e5b6094e5848e800
elishakrasz <elishashestov@gmail.com>
2021-03-14 20:30:00 +0200

    another update

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -18,34 +18,7 @@ class ShopPage extends React.Component {
   render() {
     const { match } = this.props;
 
-=======
-import React, { Component } from "react";
-import { Route } from "react-router-dom";
-import { connect } from "react-redux";
-import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
-import CollectionPage from "../collection/collection.component";
 
-import {
-  firestore,
-  convertCollectionsSnapshotToMap,
-} from "../../firebase/firebase.utils";
-import { updateCollections } from "../../redux/shop/shop.actions";
-
-class ShopPage extends Component {
-  unsubscribeFromSnapShot = null;
-
-  componentDidMount() {
-    const { updateCollections } = this.props;
-    const collectionRef = firestore.collection("collections");
-
-    collectionRef.onSnapshot(async (snapshot) => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      updateCollections(collectionsMap);
-    });
-  }
-  render() {
-    const { match } = this.props;
->>>>>>> 4330a1ee714ea6417f04d727bd1d8d6707f3e4d0
     return (
       <div className='shop-page'>
         <Route
@@ -62,7 +35,6 @@ class ShopPage extends Component {
   }
 }
 
-<<<<<<< HEAD
 const mapDispatchToProps = dispatch => ({
   fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
 });
@@ -71,11 +43,3 @@ export default connect(
   null,
   mapDispatchToProps
 )(ShopPage);
-=======
-const mapDispatchToProps = (dispatch) => ({
-  updateCollections: (collectionsMap) =>
-    dispatch(updateCollections(collectionsMap)),
-});
-
-export default connect(null, mapDispatchToProps)(ShopPage);
->>>>>>> 4330a1ee714ea6417f04d727bd1d8d6707f3e4d0
