f555b470a264c86f0da95bc90941761c852300ab
Elisha Mallard <elishashestov@gmail.com>
2021-03-21 10:30:02 +0200

    stripe backend and updates

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51ISRFvJcZtxAUyigR5wSQB7btpZq7kigN1fPIgcdQ1GF9GU5beRedTFX2fRKJdQte722abtWhHaY5gFmtg23cMky009l2BoxAc'
 
     const onToken = token => {
-        console.log(token)
-        alert('pay processed successfully!!')
-    }
+        axios({
+          url: `http://localhost:5000/payment`,
+          method: 'post',
+          data: {
+            amount: priceForStripe,
+            token: token
+          }
+        })
+          .then(response => {
+            alert('succesful payment');
+          })
+          .catch(error => {
+            console.log('Payment Error: ', error);
+            alert(
+              'There was an issue with your payment! Please make sure you use the provided credit card.'
+            );
+          });
+      };
     return (
         <StripeCheckout
             label="Pay Now"
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const { func } = require('prop-types');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+app.use(cors('*'))
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')))
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    })
+}
+
+
+app.listen(port, error => {
+    if (error) throw error;
+    console.log('Server running on port' + port);
+})
+
+app.post('/payment', (req, res) => {
+    // console.log('req', req)
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: 'usd'
+    }
+    console.log('body', body.source)
+    stripe.charges.create(body, (stripeErr, stripeRes) => {
+        if (stripeErr) {
+            res.status(500).send({ error: stripeErr})
+        } else {
+            res.status(200).send({ success: stripeRes })
+        }
+    })
+})
